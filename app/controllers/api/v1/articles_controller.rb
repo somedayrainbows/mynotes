4116568e7551a73993464991d369bf17b4a999fa
Erin Bassity <erin.bassity@cognizant.com>
2019-03-30 17:34:15 -0600

    Add show method

--- app/controllers/api/v1/articles_controller.rb
+++ app/controllers/api/v1/articles_controller.rb
@@ -7,6 +7,11 @@ module Api
         render json: {status: 'SUCCESS', message: 'Loaded articles', data: articles}, status: :ok
       end
 
+      def show
+        article = Article.find(params[:id])
+        render json: {status: 'SUCCESS', message: 'Loaded article', data: article}, status: :ok
+      end
+
     end
   end
 end
