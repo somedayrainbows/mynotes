98b684cb1ee06c62f63eb2299f4b268db16261b2
Erin Bassity <erin.bassity@cognizant.com>
2019-04-05 15:19:20 -0600

    Add notes api spec

--- spec/models/notes_spec.rb
+++ spec/models/notes_spec.rb
@@ -11,4 +11,14 @@ RSpec.describe Note, type: :model do
     it {should validate_presence_of(:body)}
   end
 
+    it "is not valid without a title" do
+      note = build(:note, title: nil, body: "A body")
+      expect(note).to_not be_valid
+    end
+
+    it "is not valid without a body" do
+      note = build(:note, title: "A title", body: nil)
+      expect(note).to_not be_valid
+    end
+
 end
---
+++ spec/requests/api/v1/notes_request_spec.rb
@@ -0,0 +1,21 @@
+require 'rails_helper'
+require 'pry'
+
+describe "Notes API" do
+  it "sends a list of notes" do
+    create_list(:note, 5)
+
+    get "/api/v1/notes"
+
+    expect(response).to be_successful
+
+    notes = JSON.parse(response.body)
+    # binding.pry
+
+    expect(notes["data"].count).to eq(5)
+    expect(notes["data"].first).to have_key("title")
+    expect(notes["data"].last).to have_key("title")
+    expect(notes["data"].first).to have_key("body")
+    expect(notes["data"].last).to have_key("body")
+  end
+end
